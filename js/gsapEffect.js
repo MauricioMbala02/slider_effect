c7a7dde394f9839cdc47f2585938fbc70ff63c21
Maurício Mbala <mauriciombalajr@gmail.com>
2023-11-20 15:27:03 +0100

    Add files via upload

---
+++ js/gsapEffect.js
@@ -0,0 +1,32 @@
+function setupSplits() {
+  
+  var tlSplitBurrowing = gsap.timeline(), 
+      SplitBurrowing = new SplitText(".titleBurrowing", {type:"words,chars"}), 
+      chars = SplitBurrowing.chars; //an array of all the divs that wrap each character
+  
+  
+  tlSplitBurrowing.from(chars, {
+    duration: 0.8,
+    opacity:0,
+    y:10,
+    ease:"circ.out",
+    stagger: 0.02,
+  scrollTrigger: {
+      trigger: ".titleBurrowing",
+  //markers:true,
+      start: "top 75%",
+     end: "bottom center",
+      scrub:1
+    }
+  //,   onComplete: () => {SplitBurrowing.revert()}
+  }, "+=0");
+  
+  // window.addEventListener('resize', function() {
+    // SplitBurrowing.revert();
+  // });
+    
+  };
+
+  ScrollTrigger.addEventListener("refresh", setupSplits);
+//ScrollTrigger.addEventListener("scrollEnd", () => SplitBurrowing.revert());
+setupSplits();
